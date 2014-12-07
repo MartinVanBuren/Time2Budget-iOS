583bf7b90dc11b3fcc7a6971c7945839c0b7e27a
Robert Kennedy <helmet0012@gmail.com>
2014-12-07 14:49:44 -0500

    Added initial project files.

---
+++ Time to Budget/Time to Budget/BudgetViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time to Budget
+//
+//  Created by Robert Kennedy on 12/7/14.
+//  Copyright (c) 2014 Arrken Games, LLC. All rights reserved.
+//
+
+import UIKit
+
+class BudgetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Time to Budget/Time to BudgetTests/Time_to_BudgetTests.swift
@@ -0,0 +1,36 @@
+//
+//  Time_to_BudgetTests.swift
+//  Time to BudgetTests
+//
+//  Created by Robert Kennedy on 12/7/14.
+//  Copyright (c) 2014 Arrken Games, LLC. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Time_to_BudgetTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
