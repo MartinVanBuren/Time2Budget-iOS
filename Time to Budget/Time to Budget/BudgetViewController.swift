2db7b0ae225df2f3af37d83dcc670361b48bfdf7
Robert Kennedy <helmet0012@gmail.com>
2014-12-08 18:12:51 -0500

    - Added tableViewDataSource support for budgetViewController - Added tableViewDelegate support for budgetViewController - Added CoreData support for budgetViewController

--- Time to Budget/Time to Budget/BudgetViewController.swift
+++ Time to Budget/Time to Budget/BudgetViewController.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
+import CoreData
 
-class BudgetViewController: UIViewController {
+class BudgetViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, NSFetchedResultsControllerDelegate {
     
+    //==================== IBOutlets ====================
+    @IBOutlet weak var tableView: UITableView!
 
+    //==================== Variables ====================
+    
+    //==================== Constants ====================
 
+    
+    //==================== Pre-Generated Methods ====================
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,10 +30,52 @@ class BudgetViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //==================== IBAction Methods ====================
     @IBAction func editButtonPressed(sender: UIBarButtonItem) {
         self.performSegueWithIdentifier("showBudgetEditor", sender: self)
     }
+    
+    //==================== UITableViewDataSource Methods ====================
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 0
+    }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+    
+    //==================== UITableViewDelegate Methods ====================
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        
+        return 25
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Default"
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    //==================== NSFetchedResultsControllerDelegate Methods ====================
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        
+    }
+    
+    //==================== Helper Methods ====================
+    
 }
 
