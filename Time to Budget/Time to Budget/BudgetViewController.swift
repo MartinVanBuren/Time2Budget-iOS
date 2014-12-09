d51d1aed05bec5a2345a24db3b5ba9e0eb449759
Robert Kennedy <helmet0012@gmail.com>
2014-12-08 22:00:31 -0500

    - Added TaskCell class - Added RecordsViewController and class - Added segue code and placeholder tableView cell code

--- Time to Budget/Time to Budget/BudgetViewController.swift
+++ Time to Budget/Time to Budget/BudgetViewController.swift
@@ -31,6 +31,19 @@ class BudgetViewController: UIViewController, UITableViewDataSource, UITableView
         // Dispose of any resources that can be recreated.
     }
     
+    //==================== Segue Preperation ====================
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showRecordsView" {
+            let recordsVC:RecordsViewController = segue.destinationViewController as RecordsViewController
+            //let indexPath = self.tableView.indexPathForSelectedRow()
+            //let thisTask = fetchedResultsController.objectAtIndexPath(indexPath!) as TaskModel
+            //detailVC.detailTaskModel = thisTask
+        }
+        else if segue.identifier == "showBudgetEditor" {
+            let budgetEditorVC:BudgetEditorViewController = segue.destinationViewController as BudgetEditorViewController
+        }
+    }
+    
     //==================== IBAction Methods ====================
     @IBAction func editButtonPressed(sender: UIBarButtonItem) {
         self.performSegueWithIdentifier("showBudgetEditor", sender: self)
@@ -39,22 +52,25 @@ class BudgetViewController: UIViewController, UITableViewDataSource, UITableView
     //==================== UITableViewDataSource Methods ====================
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         
-        return 0
+        return 1
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 0
+        return 5
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        return UITableViewCell()
+        var cell:TaskCell = tableView.dequeueReusableCellWithIdentifier("TaskCell") as TaskCell
+        cell.taskNameLabel.text = "Task Name"
+        cell.remainingTimeLabel.text = "7:45"
+        return cell
     }
     
     //==================== UITableViewDelegate Methods ====================
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        
+        performSegueWithIdentifier("showRecordsView", sender: self)
     }
     
     func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
---
+++ Time to Budget/Time to Budget/TaskCell.swift
@@ -0,0 +1,28 @@
+//
+//  TaskCell.swift
+//  Time to Budget
+//
+//  Created by Robert Kennedy on 12/8/14.
+//  Copyright (c) 2014 Arrken Games, LLC. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+    
+    @IBOutlet weak var taskNameLabel: UILabel!
+    @IBOutlet weak var remainingTimeLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
